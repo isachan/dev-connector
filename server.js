fa1f9733c90ca293681c858aa0142ba972e8ffd0
isachan <isachanjh@gmail.com>
2019-10-04 12:47:15 +0800

    Express, MongoDB Setup and User API Route & JWT Authentication (excluding Login Route)

--- server.js
+++ server.js
@@ -1,13 +1,26 @@
 //1. bring express server in
 const express = require('express');
 
+const connectDB = require('./config/db');
 //2. initiallise app variable
 const app = express();
+
+// Connect DB
+connectDB();
+
+//Init Middleware
+app.use(express.json({ extended: false }));
 
 //5. single end-point, just to test out
 //get request to /
 //callback with req, res, to send data to the browser
 app.get('/', (req, res) => res.send('API Running'));
+
+// Define Route
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 //4. will look for env called port, so when deployed to heroku, will find this, if cannot find, go to default 5000
 const PORT = process.env.PORT || 5000;
