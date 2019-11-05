bf56e67cc834456cde00f6c18f933a5aa08fc917
isachan <isachanjh@gmail.com>
2019-11-05 23:51:37 +0800

    Comment Display & Delete

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -62,7 +62,7 @@ export const removeLike = id => async dispatch => {
 // delete post
 export const deletePost = id => async dispatch => {
   try {
-    const res = await axios.delete(`/api/posts/${id}`);
+    await axios.delete(`/api/posts/${id}`);
 
     dispatch({
       type: DELETE_POST,
@@ -151,7 +151,7 @@ export const addComment = (postId, formData) => async dispatch => {
 // delete comment
 export const deleteComment = (postId, commentId) => async dispatch => {
   try {
-    const res = await axios.post(`/api/posts/comment/${postId}/${commentId}`);
+    await axios.delete(`/api/posts/comment/${postId}/${commentId}`);
 
     dispatch({
       type: REMOVE_COMMENT,
